bff0a2cec0d14fd4db12579dc15de3d1ae647924
Subhash Yadav <subhash.yadav@investwellonline.com>
2026-06-02 15:38:25 +0530

    added files

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log("start");
+// setTimeout(()=>{
+//     console.log("Settimeout")
+// },3000)
+// console.log("end")
+
+const data=fetch("https://dummyjson.com/users")
+console.log(data)
