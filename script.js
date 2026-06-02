5a72385e704bbd49ce7d28f0594f6ac26ede0781
Subhash Yadav <subhash.yadav@investwellonline.com>
2026-06-02 15:52:04 +0530

    removed files

--- script.js
+++
@@ -1,8 +0,0 @@
-// console.log("start");
-// setTimeout(()=>{
-//     console.log("Settimeout")
-// },3000)
-// console.log("end")
-
-const data=fetch("https://dummyjson.com/users")
-console.log(data)
